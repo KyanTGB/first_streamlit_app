b84b29ebab51e8c3f416f3e8942cf1deef81ab59
KyanTGB <106253442+KyanTGB@users.noreply.github.com>
2022-05-26 13:08:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('')
+streamlit.text('')
+streamlit.text('Hard-boiled Free-Range egg)
